9bf4c9adae5d8e68a8a8675e94548a71c29bca31
sacanarte@espe.edu.ec <sacanarte@espe.edu.ec>
2026-07-16 11:49:59 -0500

    Preparar API para despliegue en Vercel

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const express = required('express');
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (_req,res) =>{
-    res.send('Integración contínua funcional');
+    res.status(200).json({
+        mensaje: 'CI/CD funcionando correctamente',
+        servicio: 'API Express desplegada en Vercel'
+    });
 });
 
+app.get('/saludo/:nombre', (req,res) =>{
+    const { nombre } = req.params;
+    res.status(200).json({
+        mensaje: `Hola, ${nombre}`,
+        estado: 'API disponible'
+    });
+});
+
+app.get('/suma', (req,res) =>{
+    const  numero1  = Number(req.query.numero1);
+    const  numero2  = Number(req.query.numero2);
+
+    if(Number.isNaN(numero1) || Number.isNaN(numero2)){
+        return res.status(400).json({
+            error: 'Los parametros deber ser numericos' 
+        });
+    }
+
+    res.status(200).json({
+        numero1,
+        numero2,
+        resultado: numero1 + numero2
+    });
+});
+
+
 app.listen(PORT, () =>{
-    console.log('Servidor ejecutando en: http://localhost:${PORT}');
+    console.log(`Servidor ejecutando en: http://localhost:${PORT}`);
 });

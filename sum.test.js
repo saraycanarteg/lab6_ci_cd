ee74896965de60dc8045395663b509cf2b5809a6
sacanarte@espe.edu.ec <sacanarte@espe.edu.ec>
2026-07-20 21:44:53 -0500

    Fallo insertado resuelto

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,5 @@
 const sum = require('./sum');
 
 test('Suma 1 + 2 debe ser igual a 3', () => {
-    expect(sum(1, 2)).toBe(4); //error ocasionado
+    expect(sum(1, 2)).toBe(3); //error ocasionado
 });
